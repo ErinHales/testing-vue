f5e393def5b3224caacbe3a7d05d4906fcbd13d1
Erin Hales <erin@summa.one>
2019-03-25 15:23:09 -0600

    changing branches

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,13 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
 import Item from '../Item.vue'
-import Vue from 'vue'
-
-const Ctor = Vue.extend(Item)
-const vm = new Ctor().$mount()
 
 describe('Item.vue', () => {
   test('renders "item"', () => {
-    const Ctor = Vue.extend(Item) // #A
-    const vm = new Ctor().$mount() // #B
-    expect(vm.$el.textContent).toContain('item') // #C
+    const wrapper = shallowMount(Item)
+    expect(wrapper.text()).toContain('item') // #C
   })
 })
