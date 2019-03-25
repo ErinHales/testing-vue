abac7d95153ad7840bbccccee3b5cf24cc6173ea
Erin Hales <erin@summa.one>
2019-03-25 17:14:36 -0600

    finished writing tests

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -3,7 +3,25 @@ import Item from '../Item.vue'
 
 describe('Item.vue', () => {
   test('renders "item"', () => {
-    const wrapper = shallowMount(Item)
-    expect(wrapper.text()).toContain('item') // #C
+    const item = {
+      url: 'http://some-url.com',
+      title: 'some-title'
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+    const a = wrapper.find('a')
+    expect(a.text()).toBe(item.title)
+    expect(a.attributes().href).toBe(item.url)
+  })
+
+  test('renders a link to the item.url with item.title as text', () => {  const item =
+    {
+      title: 'some title'
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item } // #B
+    })
+    expect(wrapper.find('a').text()).toBe(item.title) // #C
   })
 })
